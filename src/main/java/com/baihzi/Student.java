b600301a33e52716e99248520851be97f41fb346
qcg <17692393933@163.com>
2018-04-27 20:43:41 +0800

    皓月次改

--- src/main/java/com/baihzi/Student.java
+++ src/main/java/com/baihzi/Student.java
@@ -3,5 +3,6 @@ package com.baihzi;
 public class Student {
     public static void main(String[] args) {
         System.out.println("zzzzzzz1.0");
+        System.out.println("haoyuegaidong");
     }
 }
